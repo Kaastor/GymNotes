e61a76363cf87b96f0d4477bea07fe3c27e14721
Kaastor <czuba.przemyslaw@gmail.com>
2017-06-22 00:12:06 +0200

    List in AddTrainingDialog is fetched from database.

--- app/src/main/java/pl/edu/wat/gymnotes/AddTrainingDialog.java
+++ app/src/main/java/pl/edu/wat/gymnotes/AddTrainingDialog.java
@@ -2,16 +2,41 @@ package pl.edu.wat.gymnotes;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.app.AlertDialog;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.util.ArraySet;
 import android.view.LayoutInflater;
 
 
-public class AddTrainingDialog extends DialogFragment {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import pl.edu.wat.gymnotes.data.ExerciseContract;
+
+
+public class AddTrainingDialog extends DialogFragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private int clickedItem;
+    private AlertDialog.Builder builder;
+    private String[] exercisesArray;
+    private List<String> exercisesList = new ArrayList<>();
+
+    private static final int ADD_TRAINING_LOADER = 0;
+    private static final String[] EXERCISE_COLUMNS = {
+            ExerciseContract.ExerciseEntry._ID,
+            ExerciseContract.ExerciseEntry.COLUMN_NAME
+    };
+    public static final int COL_EXERCISE_ID = 0;
+    public static final int COL_EXERCISE_NAME = 1;
 
     public AddTrainingDialog() {
     }
@@ -19,8 +44,11 @@ public class AddTrainingDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder = new AlertDialog.Builder(getActivity());
         // Get the layout inflater
+
+        getLoaderManager().initLoader(ADD_TRAINING_LOADER, null, this);
+
         LayoutInflater inflater = getActivity().getLayoutInflater();
         // Use the Builder class for convenient dialog construction
         builder.setView(inflater.inflate(R.layout.dialog_add_training, null));
@@ -35,14 +63,50 @@ public class AddTrainingDialog extends DialogFragment {
                         // User cancelled the dialog
                     }
                 });
-        builder.setSingleChoiceItems(R.array.exercises, clickedItem, new DialogInterface.OnClickListener() {
+        builder.setSingleChoiceItems(exercisesArray, clickedItem, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 // The 'which' argument contains the index position
                 // of the selected item
             }
         });
-
         return builder.create();
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(ADD_TRAINING_LOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri exercisesUri = ExerciseContract.ExerciseEntry.buildExercises();
+        CursorLoader cursorLoader = new CursorLoader(
+                getActivity(),
+                exercisesUri,
+                EXERCISE_COLUMNS,
+                null,
+                null,
+                null
+        );
+        Cursor data = cursorLoader.loadInBackground();
+        if (data.moveToFirst()){
+            do{
+                exercisesList.add(data.getString(data.getColumnIndex(ExerciseContract.ExerciseEntry.COLUMN_NAME)));
+            }while(data.moveToNext());
+        }
+        exercisesArray = exercisesList.toArray(new String[exercisesList.size()]);
+
+        return  cursorLoader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
     }
 }
 
